2a3681986a062fc642e44cd0c8849fc16fe4abb2
ChristianDrossel <timmi0701@gmail.com>
2020-01-10 12:30:31 +0100

    Upload project

---
+++ WPFPlayer/WPFPlayer/MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPlayer
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WPFWrapper wpfWrapper;
+
+
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            dispatcherTimer.Tick += new EventHandler(onTimerTick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+            wpfWrapper = new WPFWrapper(this, wpfPlayer);
+        }
+
+        private void Open_FileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                wpfWrapper.Open(ofd.FileName);
+                this.Title = ofd.SafeFileName;
+                sliderTime.Maximum = wpfWrapper.GetDuration();
+                sliderVolume.Value = 5;
+                sliderBalance.Value = 5;
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            wpfWrapper.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            wpfWrapper.Pause();
+        }
+
+        private void FullscreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            wpfWrapper.SetFullscreen(true);
+            wpfPlayer.Margin = new Thickness(0, 0, 0, 0);
+            myWindow.Background = new SolidColorBrush(Colors.Black);
+            setControlElementsVisible(true);  
+
+        }
+
+        void setControlElementsVisible(bool visible)
+        {
+            if (visible && wpfPlayer.Source != null)
+            {
+                Open_FileButton.Visibility = Visibility.Hidden;
+                Open_FileButton.Visibility = Visibility.Collapsed;
+
+                PlayButton.Visibility = Visibility.Hidden;
+                PlayButton.Visibility = Visibility.Collapsed;
+
+                PauseButton.Visibility = Visibility.Hidden;
+                PauseButton.Visibility = Visibility.Collapsed;
+
+                FullscreenButton.Visibility = Visibility.Hidden;
+                FullscreenButton.Visibility = Visibility.Collapsed;
+
+                sliderBalance.Visibility = Visibility.Hidden;
+                sliderBalance.Visibility = Visibility.Collapsed;
+                
+                sliderTime.Visibility = Visibility.Hidden;
+                sliderTime.Visibility = Visibility.Collapsed;
+
+                sliderVolume.Visibility = Visibility.Hidden;
+                sliderVolume.Visibility = Visibility.Collapsed;
+
+                lblStatus.Visibility = Visibility.Hidden;
+                lblStatus.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Open_FileButton.Visibility = Visibility.Visible;
+                PlayButton.Visibility = Visibility.Visible;
+                PauseButton.Visibility = Visibility.Visible;
+                FullscreenButton.Visibility = Visibility.Visible;
+                sliderBalance.Visibility = Visibility.Visible;
+                sliderTime.Visibility = Visibility.Visible;
+                sliderVolume.Visibility = Visibility.Visible;
+                lblStatus.Visibility = Visibility.Visible;
+                myWindow.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        void onTimerTick(object sender, EventArgs e)
+        {
+            sliderTime.Maximum = wpfWrapper.GetDuration();
+            if (Mouse.LeftButton != MouseButtonState.Pressed)
+            {
+                // only update, if user is currently not dragging the slider
+                double newValue = wpfWrapper.GetPosition();
+                if (newValue != -1)
+                {
+                    sliderTime.Value = newValue;
+                    lblStatus.Content = String.Format("{0:00}:{1:00} / {2:00}:{3:00}",
+                    (int)newValue / 60, (int)newValue % 60,
+                    (int)wpfWrapper.GetDuration() / 60,
+                    (int)wpfWrapper.GetDuration() % 60);
+                }
+                else
+                    lblStatus.Content = "No file selected...";
+            }
+        }
+
+
+
+        private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            wpfWrapper.SetVolume(sliderVolume.Value*10);
+            Console.WriteLine("SliderVolume: " + sliderVolume.Value*10);
+        }
+
+        private void sliderBalance_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            wpfWrapper.SetBalance(sliderBalance.Value*10);
+            Console.WriteLine("SliderBalance: " + sliderBalance.Value*10);
+        }
+
+        private void sliderTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            wpfWrapper.SetPosition(Convert.ToInt32(sliderTime.Value));
+        }
+
+        private void MouseLeftButton_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            wpfWrapper.SetFullscreen(false);
+            setControlElementsVisible(false);
+        }
+    }
+}
---
+++ WPFPlayer/WPFPlayer/WPFWrapper.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFPlayer;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows;
+
+
+
+/// <summary>
+/// TODOS       FULSCREEN funktioniert noch nicht richtig--> Button bleiben im fullscreen an selber stelle
+///                 -->Lösung vllt mit ein GRID https://www.wpf-tutorial.com/de/29/panels/the-grid-rows-columns/
+///                     https://www.youtube.com/watch?v=qiEHTgqo4yE
+///                     
+///             Video Stockt beim abspielen seit dispatcher.Timer in Konstrukter der MainWindow.xaml.cs
+/// </summary>
+
+
+
+class WPFWrapper
+{
+    System.Windows.Controls.MediaElement player;
+    MainWindow window;
+
+    public WPFWrapper(MainWindow windowHandle, MediaElement player)
+    {
+        this.player = player;
+        window = windowHandle;
+    }
+
+    /// Opens a Media File.
+    /// <param name="path">The path to the Media file as a URI.</param>
+    /// <param name="autoRun">A boolean to select the automatic start of the Video, once loaded.</param>
+    public void Open(Uri path, bool autoRun)
+    {
+        player.Source = path;
+        if (autoRun)
+            Play();
+    }
+    /// Opens a Media File and starts playback.
+    /// <param name="path">The path to the Media file as a URI.</param>
+    public void Open(Uri path)
+    {
+        Open(path, true);
+    }
+
+    /// <summary>
+    /// Opens a Media File.
+    /// </summary>
+    /// <param name="path">The path to the Media file as a String.</param>
+    /// <param name="autoRun">A boolean to select the automatic start of the Video.</param>
+    public void Open(string path, bool autoRun)
+    {
+        Open(new Uri(path, UriKind.RelativeOrAbsolute), autoRun);
+    }
+    /// <summary>
+    /// Opens a Media File and starts playback.
+    /// </summary>
+    /// <param name="path">The path to the Media file as a String.</param>
+    public void Open(string path)
+    {
+        Open(new Uri(path, UriKind.RelativeOrAbsolute), true);
+    }
+
+    /// Starts the Audio/Video.
+    public void Play()
+    {
+        if (player.Source != null) player.Play();
+    }
+    /// Pauses the Audio/Video.
+    public void Pause()
+    {
+        if (player.Source != null) player.Pause();
+    }
+    /// Stops the Audio/Video.
+    public void Stop()
+    {
+        if (player.Source != null) player.Stop();
+    }
+
+    /// Sets the Fullscreen mode. (Video only)
+    /// <param name="fullscreen">Boolean Value for the Fullscreen.</param>
+    public void SetFullscreen(bool fullscreen)
+    {
+        if (player.Source != null)
+        {
+            if (fullscreen)
+            {
+                window.WindowStyle = WindowStyle.None;
+                window.WindowState = WindowState.Maximized;
+                player.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+                player.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+            }
+            else
+            {
+                window.WindowStyle = WindowStyle.SingleBorderWindow;
+                window.WindowState = WindowState.Normal;
+                player.Width = 792;
+                player.Height = 359;
+            }
+        }
+    }
+
+    /// Returns the current Position in seconds.
+    public double GetPosition()
+    {
+        if (player.Source != null &&
+        player.NaturalDuration.HasTimeSpan)
+            return player.Position.TotalSeconds;
+        return -1;
+    }
+    /// Sets the current Position in seconds.
+    public void SetPosition(int position)
+    {
+        if (player.Source != null)
+            player.Position = new TimeSpan(0, 0, position);
+    }
+
+    /// Returns the current Volume.
+    /// 0 is silent, 100 is full Volume.
+    public double GetVolume()
+    {
+        return player.Volume * 100;
+    }
+    /// Sets the Volume of the Audio.
+    /// 0 is silent, 100 is full Volume.
+    public void SetVolume(double volume)
+    {
+        player.Volume = volume / 100;
+    }
+    /// Returns the current Balance of the Audio.
+    /// -100 is left only, +100 is right only.
+    public double GetBalance()
+    {
+        return player.Balance * 100;
+    }
+    /// Sets the Balance of the Audio.
+    /// -100 is left only, +100 is right only.
+    public void SetBalance(double balance)
+    {
+        player.Balance = balance / 100;
+    }
+
+    public double GetDuration()
+    { //return this.Duration;
+        if (player.NaturalDuration.HasTimeSpan)
+            return player.NaturalDuration.TimeSpan.TotalSeconds;
+        else
+            return 0;
+    }
+}
+
